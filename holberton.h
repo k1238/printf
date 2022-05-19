b9b173289eba99972ee440846d4d63258d5acf02
k1238 <kalebadino@gmail.com>
2022-05-19 09:54:23 +0300

    task 0

--- holberton.h
+++ holberton.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <limits.h>
 #include <stdarg.h>
@@ -113,4 +113,4 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 		unsigned char flags, int wid, int prec);
 
-#endif /* MAIN_H */
+#endif /* HOLBERTON_H */
